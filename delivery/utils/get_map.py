ea71e6485c530961949479c6f4fa1b0ca58cd354
koepm <wd503@naver.com>
2024-10-24 06:18:10 +0900

    feat: 맵 인덱스로 불러오도록 수정

--- delivery/utils/get_map.py
+++ delivery/utils/get_map.py
@@ -17,7 +17,9 @@ class MapList:
         self.map_dic = {}
         self.map_folder_path = map_path
         self.create_map()
+        self.map_key = sorted(self.map_dic.keys())
 
+    # 폴더로 부터 맵 로드 (전역경로, 곡률)
     def load_g_path_from_txt(self,f_name):
         map_data_txt = pd.read_csv(self.map_folder_path+str(f_name), sep=',', encoding='utf-8') # 파일 내용 저장 -> 2차원 리스트
 
@@ -27,6 +29,7 @@ class MapList:
 
         return g_path, g_yaw
 
+    # 초기에 파일 읽어서 Map 클래스에 데이터 저장
     def create_map(self):
         file_list = os.listdir(self.map_folder_path)
 
@@ -34,55 +37,42 @@ class MapList:
             g_path,g_yaw = self.load_g_path_from_txt(file_name)
             self.read_map(file_name,g_path,g_yaw)
 
+    # 파일 읽어서 저장
     def read_map(self, file_name, g_path, g_yaw):
         self.map_dic[file_name] = Map(file_name,g_path,g_yaw)
 
-    def chose_start_mission_map(self,utm_x, utm_y):
-        dist = math.inf
+    # 현재 진행 중인 맵의 인덱스 정보를 넣으면 불러옴
+    def chose_start_mission_map(self, map_idx):
         map_info = None
-        for i in self.map_dic:
-            kdtree = self.map_dic[i].g_kd_tree
-            near_dist, _ = kdtree.query([utm_x,utm_y])
-            if dist > near_dist:
-                dist = near_dist
-                map_info = self.map_dic[i]
+        key = self.map_key[map_idx]
+        if key in self.map_dic:
+            map_info = self.map_dic[key]
 
-        if map_info is not None:
-            return map_info
-        else:
-            return None
+        return map_info
 
-def plot_path(g_path, robot_control):
-    """
-    Function to plot the global path and the vehicle's position in a separate thread.
-    Continuously updates the vehicle's position on the plot.
-    """
-    plt.ion()  # Enable interactive mode for live updating plot
-    fig, ax = plt.subplots()
-    
-    # Plot the global path
-    g_path_x = g_path[:, 0]
-    g_path_y = g_path[:, 1]
-    ax.plot(g_path_x, g_path_y, label="Global Path", color="blue")
-    
-    # Initialize vehicle's position marker
-    vehicle_marker, = ax.plot([], [], 'ro', label="Vehicle Position")
-    target_marker, = ax.plot([], [], 'go', label="Vehicle Target")
-    
-    ax.set_xlabel("X [m]")
-    ax.set_ylabel("Y [m]")
-    ax.set_title("Global Path and Vehicle Position")
-    ax.legend()
-    ax.grid(True)
-    
-    while not rospy.is_shutdown():
-        if robot_control.current_state is not None:
-            # Update the vehicle's current position on the plot
-            vehicle_x = robot_control.current_state['x']
-            vehicle_y = robot_control.current_state['y']
 
-            vehicle_marker.set_data(vehicle_x, vehicle_y)
-        if robot_control.target_point is not None:
-            target_marker.set_data(robot_control.target_point[0],robot_control.target_point)
-        plt.draw()
-        plt.pause(0.1)
+
+if __name__ == "__main__":
+    map = "/root/ws/src/BAEMIN_Delivery_Challenge/delivery/map/"
+    map_list = MapList(map)
+    map_info = map_list.chose_start_mission_map(0)
+    key = map_list.map_key[-1]
+    last = map_list.map_dic[key].g_path
+    # print(f"{last=}")
+
+    map_idx = 0 # 이거 7미터 거리임
+
+    current_map_last_point = map_list.chose_start_mission_map(map_idx).g_path[-1]  # 마지막 좌표 (x, y)
+    print(f"{current_map_last_point[0]=}")
+
+    # 다음 맵이 있는지 확인
+    if map_idx + 1 < len(map_list.map_key):
+        # 다음 맵의 첫 좌표
+        next_map_first_point = map_list.chose_start_mission_map(map_idx+1).g_path[0]  # 첫 좌표 (x, y)
+        print(f"{next_map_first_point=}")
+
+        # 두 좌표 간의 유클리드 거리 계산
+        distance = math.sqrt((current_map_last_point[0] - next_map_first_point[0]) ** 2 +
+                            (current_map_last_point[1] - next_map_first_point[1]) ** 2)
+        
+        print(f"{distance=}")
